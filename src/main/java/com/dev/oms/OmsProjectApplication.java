fe383f92d8747c6e78654819e49bbf42b4b13433
hasemi <h171754@gmail.com>
2021-04-04 00:03:57 +0900

    [추가] 프로젝트 세팅

---
+++ src/main/java/com/dev/oms/OmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dev.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmsProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dev/oms/configures/web/Pageable.java
@@ -0,0 +1,9 @@
+package com.dev.oms.configures.web;
+
+public interface Pageable {
+
+    long getOffset();
+
+    int getSize();
+
+}
