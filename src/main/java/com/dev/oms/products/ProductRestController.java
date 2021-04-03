59968cbecf6af941b620c4d293b36665656113c4
hasemi <h171754@gmail.com>
2021-04-04 00:40:51 +0900

    [수정] API 응답 포맷 수정

--- src/main/java/com/dev/oms/products/ProductRestController.java
+++ src/main/java/com/dev/oms/products/ProductRestController.java
@@ -1,6 +1,7 @@
 package com.dev.oms.products;
 
 import com.dev.oms.errors.NotFoundException;
+import com.dev.oms.utils.ApiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+
+import static com.dev.oms.utils.ApiUtils.success;
 
 @RestController
 @RequestMapping("api/products")
@@ -22,18 +25,22 @@ public class ProductRestController {
 
     // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
     @GetMapping(path = "{id}")
-    public ProductDto findById(@PathVariable Long id) {
-        return productService.findById(id)
+    public ApiUtils.ApiResult<ProductDto> findById(@PathVariable Long id) {
+        ProductDto response =  productService.findById(id)
                 .map(ProductDto::new)
                 .orElseThrow(() -> new NotFoundException("Could not found product for " + id));
+
+        return success(response);
     }
 
     // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
     @GetMapping
-    public List<ProductDto> findAll() {
-        return productService.findAll().stream()
+    public ApiUtils.ApiResult<List<ProductDto>> findAll() {
+        List<ProductDto> responseList = productService.findAll().stream()
                 .map(ProductDto::new)
                 .collect(toList());
+
+        return success(responseList);
     }
 
 }
